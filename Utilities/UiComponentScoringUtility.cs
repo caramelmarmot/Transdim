5fb995ffcb6eda7619503d71bcaaaeb38c4f4f59
Tony Sutton <tony.sutton@renaissance.com>
2019-05-27 13:34:34 -0500

    Move each player's score into its own component, jankily

---
+++ Utilities/UiComponentScoringUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Transdim.DomainModel.GameComponents;
+
+namespace Transdim.Utilities
+{
+    public class UiComponentScoringUtility
+    {
+        internal event Action<IGameComponent, int> OnScore;
+
+        public void Score(IGameComponent component, int points)
+        {
+            OnScore?.Invoke(component, points);
+        }
+    }
+}
